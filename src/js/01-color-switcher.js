ef350e1e3a770c16d88ef9177aadea28a3df17c0
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-31 20:08:50 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleStartButton() {
+  startButton.disabled = !startButton.disabled;
+}
+
+let intervalId;
+
+startButton.addEventListener('click', () => {
+  toggleStartButton();
+
+  intervalId = setInterval(changeBodyColor, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  toggleStartButton();
+
+  clearInterval(intervalId);
+});
